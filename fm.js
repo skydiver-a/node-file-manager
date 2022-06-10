148ee8866b3d637bddab968096466f73743c4833
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-10 18:14:14 +0300

    feat: add welcome and end message functions, using username from arguments

--- fm.js
+++ fm.js
@@ -1,2 +1,26 @@
-const args = process.argv[2]
-console.log(args);
+import { argv, stdout } from 'node:process';
+
+class FileManager {
+  constructor(username) {
+    process.on('exit', () => this.exitMessage());
+    this.username = username;
+  }
+
+  init() {
+    stdout.write(`Welcome to the File Manager, ${this.username}!\n`)
+  }
+
+  exitMessage() {
+    stdout.write(`Thank you for using File Manager, ${this.username}!\n`);
+  }
+}
+
+argv.slice(2).forEach(item => {
+      if (item.slice(0, 11) === '--username=') {
+        const fm = new FileManager(item.slice(11, item.length));
+        fm.init();
+      } else {
+        stdout.write('Please enter a username.')
+        exit();
+      }
+    });
