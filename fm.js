7630276436ef7e5aec12b92b3c5fe37ceec43f05
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-12 00:33:07 +0300

    feat: add switch construction

--- fm.js
+++ fm.js
@@ -1,20 +1,69 @@
-import { argv, stdout } from 'process';
+import { argv, stdin, stdout, exit } from 'process';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { mkdir, access } from 'fs';
+import { createInterface } from 'readline';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const readLines = createInterface({
+  input: stdin,
+  output: stdout
+});
+
 class FileManager {
   constructor(username) {
-    process.on('exit', () => this.exitMessage());
+    process.on('exit', () => this.exitMessage(username));
     this.username = username;
     this.pathBase = __dirname + '/' + this.makeDirectory(username);
+    this.readLines = readLines.on('line', (line) => this.readCommandLine(line));
   }
 
   init() {
     stdout.write(`Welcome to the File Manager, ${this.username}!\n`);
 
     this.pathMessage(this.pathBase);
+  }
+
+  readCommandLine(line) {
+    switch (line.split(' ')[0]) {
+      case ('up'):
+        break;
+      case ('cd'):
+        break;
+      case ('ls'):
+        break;
+      case ('cat'):
+        break;
+      case ('add'):
+        break;
+      case ('rn'):
+        break;
+      case ('cp'):
+        break;
+      case ('mv'):
+        break;
+      case ('rm'):
+        break;
+      case ('os'):
+        break;
+      case ('hash'):
+        break;
+      case ('compress'):
+        break;
+      case ('decompress'):
+        break;
+      case ('exit'):
+        this.exit();
+        break;
+      default:
+        stdout.write('Invalid input. Please try again.');
+        break;
+    }
+  }
+
+  exit() {
+    this.readLines.close();
+    exit(0);
   }
 
   makeDirectory(dir) {
@@ -32,8 +81,8 @@ class FileManager {
     stdout.write(`You are currently in ${path}\n`);
   }
 
-  exitMessage() {
-    stdout.write(`Thank you for using File Manager, ${this.username}!\n`);
+  exitMessage(username) {
+    stdout.write(`Thank you for using File Manager, ${username}!\n`);
   }
 }
 
